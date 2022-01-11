ffe60fcf5b2f0b9cc9ba808e95a558c999e27b3c
DanLivassan <daniloxc@msn.com>
2022-01-11 13:01:01 -0300

    Add original capacity of the bag

--- q7.py
+++ q7.py
@@ -10,6 +10,7 @@ def getMaxValue(carrot_types: List, capacity: int):
     carrot_types = sorted(carrot_types, key=lambda item: item['ratio'])
     min_weight = min([carrot_type['kg'] for carrot_type in carrot_types])
     totals = {
+        "bag_capacity":capacity,
         "total_in_kg":0,
         "money_spent":0,
         "left_capacity":0
@@ -26,5 +27,5 @@ def getMaxValue(carrot_types: List, capacity: int):
     return totals
 
 
-totals = getMaxValue(carrotTypes, 30)
-print(f"You can get {totals['total_in_kg']}\nWill spent {totals['money_spent']}\nAnd left {totals['left_capacity']}kg of capacity in the bag")
+totals = getMaxValue(carrotTypes, 67)
+print(f"You can get {totals['total_in_kg']}\nWill spent {totals['money_spent']}\nAnd left {totals['left_capacity']}kg of capacity in the bag (bag have {totals['bag_capacity']}kg of capacity)")
